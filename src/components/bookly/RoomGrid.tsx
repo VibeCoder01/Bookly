1a24d559fd6d03e1f637801cd19e5f6ad88a2227
Shaun Dunmall <sdunmall@gmail.com>
2025-07-03 22:22:39 +0000

    Change the chair icon to an office chair.

--- src/components/bookly/RoomGrid.tsx
+++ src/components/bookly/RoomGrid.tsx
@@ -108,20 +108,21 @@ export function RoomGrid({ roomsWithUsage, config }: RoomGridProps) {
                         <div className={cn("flex flex-col items-center flex-grow justify-center", styles.iconPadding)}>
                             <div className="flex items-center gap-x-2">
                                 <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width={styles.iconSize}
-                                height={styles.iconSize}
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width={styles.iconSize}
+                                    height={styles.iconSize}
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
                                 >
-                                <path d="M7 21h10" />
-                                <path d="M10 21v-5a2 2 0 0 1 4 0v5" />
-                                <path d="M17 3H7a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2Z" />
-                                <path d="M7 8h10" />
+                                    <path d="M16.4 20a2 2 0 0 0 2-2V13a2 2 0 0 0-2-2H7.6a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2Z"/>
+                                    <path d="M7 11V4a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v7"/>
+                                    <path d="M12 20v2"/>
+                                    <path d="M6 11h12"/>
+                                    <path d="M10 22h4"/>
                                 </svg>
                                 <span className={cn("font-semibold", styles.capacityText)}>x {room.capacity}</span>
                             </div>
